a2620dca7410878138b324dd982beb7ebd3d0c06
Victor Ama <amavictor47@gmail.com>
2023-06-13 14:35:54 +0100

    added navigation files

--- src/infrastructure/navigation/bottom.navigation.js
+++ src/infrastructure/navigation/bottom.navigation.js
@@ -1,10 +1,14 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { HomeScreen } from '../../screens/sharedScreens/mainScreens/homeScreen';
 
 
-export const MainNavigation = () => {
+export const BottomNavigation = () => {
 
-    
+    const Tab = createBottomTabNavigator()
 
     return (
-        
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={HomeScreen} />
+        </Tab.Navigator>
     )
 }
